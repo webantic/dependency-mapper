3b6b76181ec6c6c602312ee1666eb5548fee6d13
Carl Evans <carl@webantic.co.uk>
2017-08-28 23:35:38 +0100

    Remove dep on webantic/util

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-var array = require('@webantic/util').array
 var path = require('path')
 
-module.exports = function processPackageJson (json, currentPrefix) {
-  cPrefix = currentPrefix
-  let meteorDeps = extractMeteorDeps(json)
+module.exports = function (absPath) {
+  try {
+    var json = require(path.join(absPath, 'package.json'))
+    return processPackageJson(json, absPath)
+  } catch (ex) {
+    console.error(ex)
+    return {}
+  }
+}
+
+function forceArray(input) {
+  return Array.isArray(input) ? input : [input]
+}
+
+function addToSetInPlace(set, item) {
+  if (set.indexOf(item) === -1) {
+    set.push(item)
+    return true
+  }
+
+  return false
+}
+
+function processPackageJson (json, currentPrefix) {
+  var meteorDeps = extractMeteorDeps(json)
 
   if ('dependencies' in json) {
-    for (let moduleName in json.dependencies) {
+    for (var moduleName in json.dependencies) {
       if (json.dependencies.hasOwnProperty(moduleName) && moduleName.indexOf('@webantic/') === 0) {
-        const newPrefix = path.join(currentPrefix, 'node_modules', moduleName)
+        var newPrefix = path.join(currentPrefix, 'node_modules', moduleName)
         meteorDeps = mergeDependencyMaps(meteorDeps, processPackageJson(getPackageJson(moduleName, currentPrefix), newPrefix))
       }
     }
@@ -18,11 +39,11 @@ module.exports = function processPackageJson (json, currentPrefix) {
 }
 
 function mergeDependencyMaps (map1, map2) {
-  for (let key in map2) {
+  for (var key in map2) {
     if (map2.hasOwnProperty(key)) {
       if (key in map1) {
-        map1[key] = array.forceArray(map1[key])
-        array.forceArray(map2[key]).forEach((value) => array.addToSetInPlace(map1[key], value))
+        map1[key] = forceArray(map1[key])
+        forceArray(map2[key]).forEach((value) => addToSetInPlace(map1[key], value))
       } else {
         map1[key] = map2[key]
       }
@@ -33,7 +54,7 @@ function mergeDependencyMaps (map1, map2) {
 }
 
 function getPackageJson (moduleName, currentPrefix) {
-  const absPath = path.resolve(currentPrefix, 'node_modules', moduleName, 'package.json')
+  var absPath = path.resolve(currentPrefix, 'node_modules', moduleName, 'package.json')
   try {
     return process.mainModule.require(absPath)
   } catch (ex) {
